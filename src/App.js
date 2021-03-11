c9e31fbb5ad1d4de9d4d1682f7183b13d00c1645
noomenkrichen <nkrichen@outlook.com>
2021-03-11 09:57:44 +0100

    completed react state checkpoint

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
+
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    fullName: "Noomen Krichen",
+    bio: "I like coding",
+    imgSrc: "pic.jpg",
+    profession: "Web developer",
+    hide: true
+  }
+
+  handleClick = () => this.setState ({hide:false})
+
+  render() {
+    return(
+      <>
+      <div hidden={this.state.hide} style={{textAlign:"center"}}>
+        <h1>My User Profile</h1>
+        <img src={this.state.imgSrc} alt="profile"/>
+        <p><b>Full Name:</b> {this.state.fullName}</p>
+        <p><b>Biography:</b> {this.state.bio}</p>
+        <p><b>Profession:</b> {this.state.profession}</p>
+      </div>
+      <button onClick={this.handleClick}>Show my profile</button>
+      </>
+    )
+  }
 }
 
 export default App;
