7c449f6cc2375331e1c606975f8642526a48790b
noomenkrichen <nkrichen@outlook.com>
2021-03-11 10:22:31 +0100

    improved the hide show toggle button function

--- src/App.js
+++ src/App.js
@@ -8,23 +8,23 @@ class App extends React.Component {
     bio: "I like coding",
     imgSrc: "pic.jpg",
     profession: "Web developer",
-    hide: true
+    hide: false
   }
 
-  handleClick = () => this.setState ({hide:false})
+  handleClick = () => this.setState ({hide:!this.state.hide})
 
   render() {
     return(
-      <>
-      <div hidden={this.state.hide} style={{textAlign:"center"}}>
-        <h1>My User Profile</h1>
-        <img src={this.state.imgSrc} alt="profile"/>
-        <p><b>Full Name:</b> {this.state.fullName}</p>
-        <p><b>Biography:</b> {this.state.bio}</p>
-        <p><b>Profession:</b> {this.state.profession}</p>
+      <div className="App" >
+        <div hidden={this.state.hide} >
+          <h1>My User Profile</h1>
+          <img src={this.state.imgSrc} alt="profile"/>
+          <p><b>Full Name:</b> {this.state.fullName}</p>
+          <p><b>Biography:</b> {this.state.bio}</p>
+          <p><b>Profession:</b> {this.state.profession}</p>
+        </div>
+        <button onClick={this.handleClick}>Click here to hide or show my profile</button>
       </div>
-      <button onClick={this.handleClick}>Show my profile</button>
-      </>
     )
   }
 }
